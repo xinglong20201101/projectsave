8f4a292046153ef95310cda0844d3dcf6cbda911
青恩 蔡 <andy77122@hotmail.com>
2020-12-24 23:03:51 +0800

    用MVC作首頁框架

---
+++ XingLongWeb/XingLongWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XingLongWeb.Models;
+
+namespace XingLongWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        LittleBoxContent item = new LittleBoxContent();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<LittleBoxContent> list = new List<LittleBoxContent>()
+            {
+                new LittleBoxContent(){ Title="服務內容", ImageName="照片要用陣列處理" },
+                new LittleBoxContent(){ Title="服務案例", ImageName="照片要用陣列處理" },
+                new LittleBoxContent(){ Title="塔位銷售", ImageName="照片要用陣列處理" },
+                new LittleBoxContent(){ Title="政府資訊", ImageName="照片要用陣列處理" },
+            };
+            ViewData["Severvice"] = list[0];
+            ViewData["Case"] = list[1];
+            ViewData["Tower"] = list[2];
+            ViewData["Government"] = list[3];
+            return View();
+        }
+       
+    }
+}
---
+++ XingLongWeb/XingLongWeb/Models/LittleBoxContent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XingLongWeb.Models
+{
+    public class LittleBoxContent
+    {
+        public string Title { get; set; }
+
+        public string ImageName { get; set; }
+
+        public int Count { get; set; }
+    }
+}
